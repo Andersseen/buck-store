dbdf0e05601bde194bec16c3b13dfdf0e5a74433
andersseen <andriipap01@gmail.com>
2025-08-24 15:55:10 +0200

    Add assets config

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -3,13 +3,13 @@ import {
   provideBrowserGlobalErrorListeners,
 } from "@angular/core";
 import { provideRouter } from "@angular/router";
-
 import { provideHttpClient } from "@angular/common/http";
 import {
   provideClientHydration,
   withEventReplay,
 } from "@angular/platform-browser";
 import routes from "./routes";
+import { provideAppConfig } from "@shared/services/config-api";
 
 const appConfig: ApplicationConfig = {
   providers: [
@@ -17,6 +17,7 @@ const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideHttpClient(),
+    provideAppConfig(),
   ],
 };
 export default appConfig;
